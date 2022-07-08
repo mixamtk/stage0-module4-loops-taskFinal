e8f7d3420b0ad1619132a51d7632da6238208fc6
mixamtk <70889784+mixamtk@users.noreply.github.com>
2022-07-08 17:57:46 +0300

    Update HalfPyramid.java

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,7 +2,15 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
-
+        int count = cathetusLength;
+        for (int i = 1; i <= cathetusLength; i++) {
+            for (int a = 1; a <= cathetusLength; a++) {
+                if (cathetusLength-count<a) System.out.print("*");
+                else System.out.print(" ");
+                if (a == cathetusLength) System.out.println();
+            }
+            count--;
+        }
     }
 
     public static void main(String[] args) {
